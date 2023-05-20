7d2cea0e0a94d9400a0fa22ff40488c3fccdcd30
Tahsin Sumiya <Tahsinadity187@gmail.com>
2023-05-20 23:41:15 +0600

    first commit

---
+++ src/components/Editor.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+import 'codemirror/mode/xml/xml';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/css/css';
+import { Controlled as ControlledEditor } from 'react-codemirror2';
+import '../index.css'
+export default function Editor(props) {
+    const {
+        language,
+        displayName,
+        value,
+        onChange
+      } = props
+
+      const [open, setOpen] = useState(true)
+      function handleChange(editor, data, value) {
+        onChange(value)
+      }
+  return (
+    <div>
+      <div>
+        html
+        <button 
+          type="button"
+          className="expand-collapse-btn"
+          onClick={() => setOpen(prevOpen => !prevOpen)}
+        > BUTTON </button>
+      </div>
+      <ControlledEditor 
+                onBeforeChange={handleChange}
+                value={value}
+                className="controlled-editor"
+                options={{
+                    lineWrapping: true,
+                    lint: true,
+                   
+                    lineNumbers: true,
+                    theme: 'material'
+                }}
+            />
+    </div>
+  )
+}
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Code from './Code'
+export default function Home() {
+  return (
+    <>
+      <Code/>
+    </>
+  )
+}
